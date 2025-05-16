8b85e277b11d514a97321f50126ed3a9860700e4
Aramis Hernandez <ah1027406@wcupa.edu>
2025-05-15 21:59:04 -0400

    Finished Mesh Class

--- include/mesh.h
+++ include/mesh.h
@@ -6,10 +6,12 @@
 #include <string>
 #include <vector>
 
+#include <Shader.h>
+
 struct Vertex {
 	glm::vec3 Position;
 	glm::vec3 Normal;
-	glm::vec3 TexCords;
+	glm::vec3 TexCoords;
 };
 
 struct Texture {
@@ -33,7 +35,39 @@ public:
 	}
 
 	//Implement actual shader loader class
-	//void Draw(Shader &shader);
+	void Draw(Shader& shader) {
+		/*Textures will go by the naming convection example
+		uniform sampler2D texture_diffuse1;
+		uniform sampler2D texture_diffuse2;
+		uniform sampler2D texture_diffuse3;
+		uniform sampler2D texture_specular1;
+		uniform sampler2D texture_specular2;
+		etc...
+		*/
+
+		unsigned int diffuseNr = 1;
+		unsigned int specularNr = 1;
+
+		for (int i = 0; i < textures.size(); i++) {
+			glActiveTexture(GL_TEXTURE0 + i); //Active the proper texture before we bind.
+
+			std::string number;
+			std::string name = textures[i].type; //will give us either diffuse or specular.
+			if (name == "texture_diffuse")
+				number = std::to_string(diffuseNr++);
+			else if (name == "texture_specular")
+				number = std::to_string(specularNr++);
+
+			shader.setInt(("matieral." + name + number).c_str(), i);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+		}
+		glActiveTexture(GL_TEXTURE0);
+
+		//Draw our mesh
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
 
 private:
 	//Our render data
@@ -60,9 +94,11 @@ private:
 
 		//Vertex Normals
 		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
 
 		//Vertexx Texture Cords
 		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
 		
 
