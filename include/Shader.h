d4da31a55fdb260dead75d90b85f3fcf554a8c2f
Aramis Hernandez <ah1027406@wcupa.edu>
2025-05-15 21:58:50 -0400

    Update Shader.h
    Added setInt for texture binding on mesh class

--- include/Shader.h
+++ include/Shader.h
@@ -86,6 +86,11 @@ public:
 		return glGetUniformLocation(this->get(), uniformName.c_str());
 	}
 
+	void setInt(const std::string& name, int value) const
+	{
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
 
 
 private:
