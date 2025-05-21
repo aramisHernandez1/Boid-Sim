4bf5183a23f9c103c27f21d047b78fc65c2bdc4b
Aramis Hernandez <ah1027406@wcupa.edu>
2025-05-21 16:16:52 -0400

    Update Shader.h
    Added Vec3 Helper method for setting uniforms

--- include/Shader.h
+++ include/Shader.h
@@ -97,6 +97,10 @@ public:
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 	}
 
+	void setVec3(const std::string& name, const glm::vec3& value) {
+		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+
 	void setMat4(const std::string& name, const glm::mat4& mat) const
 	{
 		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
